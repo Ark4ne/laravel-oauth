8bdfbe033b36c7996a59c2f3725754018fa8d48e
Guillaume Allegret <gui.allegret@gmail.com>
2016-04-16 18:30:30 +0200

    Add Class docBlocks with @see & @method static for IDE autoCompletion

--- src/Facades/OAuth.php
+++ src/Facades/OAuth.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class OAuth
+ *
+ * @package Jenssegers\OAuth\Facades
+ *          
+ * @see \Jenssegers\OAuth\OAuth 
+ * @method static \OAuth\Common\Service\AbstractService consumer($service, $url = null, $scope = null, $baseUri = null, $version = null)
+ * @method static \OAuth\Common\Service\AbstractService service($service, $url = null, $scope = null, $baseUri = null, $version = null)
+ * @method static setHttpClient($client)
+ */
 class OAuth extends Facade {
 
     /**
